d23fd38d01a95ee62fdaf5a2e67c9ae2329ebcbe
heaks <victor.vyshnevsky64@gmail.com>
2018-03-16 02:59:22 +0200

    ok

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../pages/main/App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
